9b766bbba5e8aafcb74901076801cb13e01bbc98
Etta-Diego <onyinyechiikediego.gmail.com>
2024-03-08 11:42:09 +0100

    refixed 9-full_creation.sql file

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,9 +2,9 @@
 -- hbtn_0c_0 in the MySQL server and add multiples rows.
 
 CREATE IF NOT EXISTS second_table (
-	"id" INT,
-	"name" VARCHAR(256)
-	"score" INT
+	id INT,
+	name VARCHAR(256)
+	score INT
 	);
 INSERT INTO second_table (
 	id, name, score)
