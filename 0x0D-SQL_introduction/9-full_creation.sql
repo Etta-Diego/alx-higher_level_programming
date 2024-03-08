7194265c2d27f5f5378aa06e1f7ff9a76a6d3049
Etta-Diego <onyinyechiikediego.gmail.com>
2024-03-08 02:06:23 +0100

    removed ''x in 9-full_creation.sql file

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,8 +6,8 @@ CREATE IF NOT EXISTS second_table (
 	"name" VARCHAR(256)
 	"score" INT
 	);
-INSERT INTO "second_table" (
-	"id", "name", "score")
+INSERT INTO second_table (
+	id, name, score)
 VALUES (1, "John", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
