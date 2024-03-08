ddaa01e32a5335e1ddc78e4becd5ede294396ec4
Etta-Diego <onyinyechiikediego.gmail.com>
2024-03-08 22:47:54 +0100

    refixed 100-move_to_utf8.sql file

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,4 @@
 -- (utf8mb4, collate utf8mb4_unicode_ci) in the MySQL server.
 USE hbtn_0c_0;
 ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4, collate utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
