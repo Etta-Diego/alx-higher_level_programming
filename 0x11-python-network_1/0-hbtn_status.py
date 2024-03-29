909f59f8a69624889f101df0e96e11dbc39288fc
Etta-Diego <onyinyechiikediego.gmail.com>
2024-03-29 01:29:37 +0100

    all tasks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
-""" This python script fetches https://alx-intranet.hbtn.io/status using the package urllib"""
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as feedback:
-    hypertext = feedback.read()
-print("Body response:")
-print("\t- type:", type(hypertext))
-print("\t- content:", hypertext)
-print("\t- utf8 content:", hypertext.decode("utf-8"))
+
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
